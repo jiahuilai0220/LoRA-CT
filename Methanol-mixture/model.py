ac6f7d0c37b96b29738c57d27fb068a9a66f4a4d
jiahuilai <232311025@csu.edu.cn>
2025-01-13 17:00:14 +0800

    Re-add Methanol-mixture folder using Git LFS

--- Methanol-mixture/model.py
+++ Methanol-mixture/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c34b6e8e7400128d980a981bdeae15e5abe12e95b6aec3382f400bbbad1504d
-size 1369
+oid sha256:a8a302ecb0dd8f54042997b7f27e519b757dee72f842540bbef01b3208db2484
+size 1401
