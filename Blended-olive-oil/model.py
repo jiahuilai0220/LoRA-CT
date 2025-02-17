77e19a4108d6c50b57e51e48a204c60b86392a8f
jiahuilai <232311025@csu.edu.cn>
2025-02-17 16:51:10 +0800

    update

--- Blended-olive-oil/model.py
+++ Blended-olive-oil/model.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 class CNN(nn.Module):
     def __init__(self):
         super(CNN,self).__init__()
-        self.Conv1d_1=nn.Conv1d(in_channels=1, out_channels=16, kernel_size=5, stride=2)
+        self.Conv1d_1=nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, stride=2)
         self.BatchNorm1d_1=nn.BatchNorm1d(16)
         self.RELU_1=nn.LeakyReLU()
-        self.Conv1d_2=nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=3)
+        self.Conv1d_2=nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=1)
         self.BatchNorm1d_2=nn.BatchNorm1d(32)
         self.RELU_2=nn.LeakyReLU()
-        self.Conv1d_3=nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, stride=1)
+        self.Conv1d_3=nn.Conv1d(in_channels=32, out_channels=64, kernel_size=1, stride=3)
         self.BatchNorm1d_3=nn.BatchNorm1d(64)
         self.RELU_3=nn.LeakyReLU()
-        self.AdaptiveMaxPool1d=nn.AdaptiveMaxPool1d(100) #参数表示输出的特征维度
+        self.AdaptiveMaxPool1d=nn.AdaptiveMaxPool1d(120) #参数表示输出的特征维度
         self.Flatten=nn.Flatten()
-        self.Linear=nn.Linear(64*100,32*100)
-        self.Linear2=nn.Linear(32*100,1)
+        self.Linear=nn.Linear(64*120,32*120)
+        self.Linear2=nn.Linear(32*120,1)
     def forward(self,input):
         output=self.Conv1d_1(input)
         output=self.BatchNorm1d_1(output)
